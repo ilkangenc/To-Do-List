d6441b731baa4bd855f5cb196965f4448b3828c8
ilkangenc <gencilkan@gmail.com>
2020-12-13 19:15:09 +0300

    components added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+
+  const [inputText,setinputText] = useState("");
+  const [todos, setTodos] = useState([])
   return (
     <div className="App">
       <header>
         <h1>Todo List</h1>
       </header>
+      <Form inputText ={inputText} todos={todos} setTodos={setTodos} setinputText= {setinputText} />
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,1 +1,15 @@
-ECHO is on.
+
+import React from 'react';
+import Todo from './Todo.js'
+const TodoList = () => {
+
+    return(
+        <div className="todo-container">
+        <ul className="todo-list">
+        <Todo /></ul>
+      </div>
+    )
+}
+
+
+export default TodoList;
