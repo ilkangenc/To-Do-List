88905cf1837c5dc2d469b1e463c8b7c1a67479c4
ilkangenc <gencilkan@gmail.com>
2020-12-15 20:44:22 +0300

    added filter functionalities

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const Form = ({setinputText,todos,setTodos, inputText}) => {
+const Form = ({setinputText,todos,setTodos, inputText,setStatus,filteredTodos}) => {
     const inputTextHandler =(e)=> {
         console.log(e.target.value)
         setinputText(e.target.value);
@@ -14,6 +14,11 @@ const Form = ({setinputText,todos,setTodos, inputText}) => {
         ]);
         setinputText("");
     };
+    const statusHandler =(e) => {
+        console.log(e.target.value);
+        setStatus(e.target.value);
+      
+    }
     return(
         <form>
             <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
@@ -21,7 +26,7 @@ const Form = ({setinputText,todos,setTodos, inputText}) => {
                 <i className="fas fa-plus-square"></i>
             </button>
             <div className="select">
-                <select name="todos" className="filter-todo">
+                <select onChange={statusHandler} name="todos" className="filter-todo">
                     <option value="all">All</option>
                     <option value="completed">Completed</option>
                     <option value="uncompleted">Uncompleted</option>
